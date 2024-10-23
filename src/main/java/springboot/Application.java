e9b317c7a4495ec07d9b6f8ed259b04ac9e03eb2
Bekzod Akbarov <stx.webdev@gmail.com>
2024-10-23 14:50:08 +0500

    creating Beans with @Bean

--- src/main/java/springboot/Application.java
+++ src/main/java/springboot/Application.java
@@ -1,13 +1,35 @@
 package springboot;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
+
+        System.out.println(beanDefinitionNames.length);
+    }
+
+    @Bean("red-bean-object")
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    @SessionScope
+    @ApplicationScope
+    public String redBean() {
+        return "Manchester United";
     }
 
 }
