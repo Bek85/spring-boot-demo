004548ed83b98696c79d5b1338e46272616d7681
Bekzod Akbarov <stx.webdev@gmail.com>
2024-10-23 15:08:02 +0500

    dependency injection

--- src/main/java/springboot/Application.java
+++ src/main/java/springboot/Application.java
@@ -1,6 +1,8 @@
 package springboot;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -18,18 +20,41 @@ public class Application {
         String[] beanDefinitionNames = context.getBeanDefinitionNames();
 
         for (String beanDefinitionName : beanDefinitionNames) {
-            System.out.println(beanDefinitionName);
+//            System.out.println(beanDefinitionName);
         }
 
         System.out.println(beanDefinitionNames.length);
     }
 
-    @Bean("red-bean-object")
-    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    @SessionScope
-    @ApplicationScope
+    @Bean() // By default, Bean objects are Singletons
+//    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // Can be changed from singleton to other
+//    @SessionScope
+//    @ApplicationScope
     public String redBean() {
         return "Manchester United";
+    }
+
+    @Bean
+    public String blueBean() {
+        return "Chelsea";
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(String redBean , String blueBean) {
+        return args -> {
+            System.out.println("Hello from CommandLineRunner");
+            System.out.println(redBean);
+            System.out.println(blueBean);
+        };
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner2(String redBean , String blueBean) {
+        return args -> {
+            System.out.println("Hello from CommandLineRunner");
+            System.out.println(redBean);
+            System.out.println(blueBean);
+        };
     }
 
 }
