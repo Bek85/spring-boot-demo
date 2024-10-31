6735b35099dc6dd3f13e32fe7d4e4962d0a3d975
Bekzod Akbarov <stx.webdev@gmail.com>
2024-10-31 10:35:11 +0500

    Filters in practice

---
+++ src/main/java/com/alex/LoggingFilter.java
@@ -0,0 +1,34 @@
+package com.alex;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class LoggingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain)
+            throws IOException, ServletException {
+
+        System.out.println("LoggingFilter");
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        request.getHeaderNames().asIterator().forEachRemaining(n -> System.out.println(n + ": " + request.getHeader(n)));
+
+        String token = request.getHeader("token");
+
+        if(token == null) {
+            httpServletResponse.sendError(403);
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+}
