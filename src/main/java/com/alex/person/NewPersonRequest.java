0b1c5d48f8938d8687eeb3859fb1925edc192423
Bekzod Akbarov <stx.webdev@gmail.com>
2024-11-02 11:59:48 +0500

    include binding errors

--- src/main/java/com/alex/person/NewPersonRequest.java
+++ src/main/java/com/alex/person/NewPersonRequest.java
@@ -2,13 +2,16 @@ package com.alex.person;
 
 import com.alex.validation.Foo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record NewPersonRequest(
-    @NotEmpty @Foo String name,
+    @NotEmpty String name,
     @Min(16) Integer age,
     @NotNull Gender gender,
+    @Email String email,
     @JsonIgnore String password) {
 }
