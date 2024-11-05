d4bf3c5417b7c4b4cb8e4a63d48230538b24e6d4
Bekzod Akbarov <stx.webdev@gmail.com>
2024-11-05 16:05:14 +0500

    using @Column annotation

--- src/main/java/com/alex/student/Student.java
+++ src/main/java/com/alex/student/Student.java
@@ -1,10 +1,6 @@
 package com.alex.student;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.*;
 
 @Entity(name = "student")
 public class Student {
@@ -12,10 +8,19 @@ public class Student {
   @Id
   @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+  @Column(name="id", updatable = false)
   private Long id;
+
+  @Column(name = "first_name", nullable = false, columnDefinition = "TEXT")
   private String firstName;
+
+  @Column(name = "last_name", nullable = false, columnDefinition = "TEXT")
   private String lastName;
+
+  @Column(name = "email", nullable = false, columnDefinition = "TEXT", unique = true)
   private String email;
+
+  @Column(name = "age", nullable = false)
   private Integer age;
 
   public Student(String firstName, String lastName, String email, Integer age, Long id) {
