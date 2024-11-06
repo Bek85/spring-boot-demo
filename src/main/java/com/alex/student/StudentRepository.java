604553d4681e6312dbd391c360a2661b564c59c0
Bekzod Akbarov <stx.webdev@gmail.com>
2024-11-06 11:45:57 +0500

    @Query annotation in practice

--- src/main/java/com/alex/student/StudentRepository.java
+++ src/main/java/com/alex/student/StudentRepository.java
@@ -1,11 +1,16 @@
 package com.alex.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+  @Query("SELECT s FROM student s WHERE s.email = ?1")
   Optional<Student> findByEmail(String email);
 
+  @Query("SELECT s FROM student s WHERE s.firstName = ?1 AND s.age = ?2")
   List<Student> findStudentsByFirstNameEqualsAndAgeEquals(String firstName, Integer age);
+
 }
