9e963b848862579114c1d8be2bb3481ae07d6198
Bekzod Akbarov <stx.webdev@gmail.com>
2024-11-06 11:54:29 +0500

    Native queries

--- src/main/java/com/alex/student/StudentRepository.java
+++ src/main/java/com/alex/student/StudentRepository.java
@@ -7,8 +7,14 @@ import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+  // ! JPQL Query
   @Query("SELECT s FROM student s WHERE s.email = ?1")
   Optional<Student> findByEmail(String email);
+
+  // ! Native Query
+  // @Query(value = "SELECT * FROM student s WHERE s.email = ?1", nativeQuery =
+  // true)
+  // Optional<Student> findByEmail(String email);
 
   @Query("SELECT s FROM student s WHERE s.firstName = ?1 AND s.age = ?2")
   List<Student> findStudentsByFirstNameEqualsAndAgeEquals(String firstName, Integer age);
