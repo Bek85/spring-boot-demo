ffc93f43874ecbd7874be4884c6a89694651f9d0
Bekzod Akbarov <stx.webdev@gmail.com>
2024-11-09 16:28:00 +0500

    UserDetails and BcryptPasswordEnconder

--- src/main/java/com/alex/security/SecurityConfig.java
+++ src/main/java/com/alex/security/SecurityConfig.java
@@ -1,9 +1,15 @@
 package com.alex.security;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
 
@@ -16,6 +22,22 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
         .httpBasic(Customizer.withDefaults())
         .build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+    UserDetails userDetails = User.withUsername("alex")
+        .password(passwordEncoder.encode("12345"))
+        .roles("ADMIN")
+        .build();
+
+    return new InMemoryUserDetailsManager(userDetails);
+
+  }
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 
 }
